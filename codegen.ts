644e24c4758eb0d77b98e6d9545fdb1ee2c736df
Jaime Ramos <jrossetes@gmail.com>
2023-08-05 10:33:59 -0500

    watch changes for graphql schema types

--- codegen.ts
+++ codegen.ts
@@ -3,6 +3,7 @@ import type { CodegenConfig } from "@graphql-codegen/cli";
 const config: CodegenConfig = {
   overwrite: true,
   schema: "./src/api/schemas/*.graphql",
+  watch: true,
   generates: {
     "src/__generated__/resolvers-types.ts": {
       plugins: ["typescript", "typescript-resolvers"],
